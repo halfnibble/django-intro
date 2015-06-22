41c4944646398e384de4bb4af7400f4def41da14
halfnibble <josh@halfnibble.com>
2015-06-22 14:22:31 -0700

    Add Send Invoice feature.

--- freelance/invoices/urls.py
+++ freelance/invoices/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     url(r'^$', views.InvoiceList.as_view(), name='invoices'),
     url(r'^(?P<pk>\d+)/$', views.InvoiceDetail.as_view(),
         name='invoice_detail'),
+    url(r'^send/(?P<pk>\d+)/$', views.SendInvoice.as_view(),
+        name='send_invoice'),
 ]
--- freelance/invoices/views.py
+++ freelance/invoices/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from datetime import datetime
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.core.mail import send_mail
+from django.core.urlresolvers import reverse
+from django.contrib import messages
 
 from .models import Invoice
 
@@ -12,4 +16,47 @@ class InvoiceList(generic.ListView):
 class InvoiceDetail(generic.DetailView):
     model = Invoice
     context_object_name = 'invoice'
+
+
+class SendInvoice(generic.RedirectView):
+    permanent = False
+    query_string = True
+    pattern_name = 'invoice_detail'
+
+    def send_invoice(self, invoice):
+        msg = ('Please review the following invoice:\n' +
+               'Invoice #{0}: {1}\n\n' + 
+               'Invoiced on {2}.').format(
+                    invoice.id,
+                    self.request.build_absolute_uri(
+                        reverse('invoice_detail', kwargs={'pk': invoice.id})
+                    ),                    
+                    invoice.invoiced_date
+                )
+
+        send_mail(
+            subject='Invoice #{0}'.format(invoice.id),
+            message=msg,
+            from_email='no-reply@localhost',
+            recipient_list=[invoice.client.email],
+            fail_silently=False
+        )
+
+    def get_redirect_url(self, *args, **kwargs):
+        invoice = get_object_or_404(Invoice, pk=kwargs['pk'])
+
+        if invoice.status == 'PEND':
+            invoice.status = 'INV'
+        if invoice.invoiced_date is None:
+            invoice.invoiced_date = datetime.now()
+        invoice.save()
+
+        self.send_invoice(invoice=invoice)
+
+        messages.info(self.request,
+            "Invoice #{0} sent to client.".format(invoice.id)
+        )
+
+        return super(SendInvoice, self).get_redirect_url(*args, **kwargs)
+
 
